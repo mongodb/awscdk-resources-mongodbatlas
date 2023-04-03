d86245a0b4862d0d34a508341d6fa31e32bbf8f2
maastha <122359335+maastha@users.noreply.github.com>
2023-04-03 14:17:30 +0100

    [INTMDB-712]: Add unit tests for atlas-private-endpoint l3 (#3)

---
+++ test/l3-resources/atlas-private-endpoint/index.test.ts
@@ -0,0 +1,131 @@
+import { App, Stack } from 'aws-cdk-lib';
+import { atlasBasicPrivateEndpoint } from '../../../src';
+import { AtlasBasicProps } from '@mongodbatlas-awscdk/atlas-basic';
+import { PrivateEndpointProps } from '../../../src/l3-resources/atlas-basic-private-endpoint';
+import { Match, Template } from 'aws-cdk-lib/assertions';
+
+const RESOURCE_NAME_PROJECT = 'MongoDB::Atlas::Project';
+const RESOURCE_NAME_CLUSTER = 'MongoDB::Atlas::Cluster';
+const RESOURCE_NAME_DB_USER = 'MongoDB::Atlas::DatabaseUser';
+const RESOURCE_NAME_PROJECT_IP_ACCESS_LIST = 'MongoDB::Atlas::ProjectIpAccessList';
+const RESOURCE_NAME_PRIVATE_ENDPOINT = 'MongoDB::Atlas::PrivateEndpoint';
+const ORG_ID = 'testProjectId';
+const PROJECT_NAME = 'test';
+const INSTANCE_SIZE = 'M30';
+const REGION = 'US_EAST_1';
+const DATABASE_USER_NAME = 'atlas-user';
+const ACCESS_LIST_IP_ADDR = '10.10.0.0/24';
+const ACCESS_LIST_COMMENT = 'Open subnets';
+const AWS_VPC_ID = 'vpc-112233445566';
+const AWS_SUBNET_ID = '"subnet-112233445566';
+const ADMIN_DB = 'admin';
+const ROLE_NAME = 'atlasAdmin';
+const PWD = 'atlas-pwd';
+
+
+test('AtlasBasicPrivateEndpoint construct should contain default properties', () => {
+  const mockApp = new App();
+  const stack = new Stack(mockApp);
+
+  const atlasBasicProps: AtlasBasicProps = {
+    clusterProps: {
+      replicationSpecs: [
+        {
+          numShards: 3,
+          advancedRegionConfigs: [{
+            regionName: REGION,
+            electableSpecs: {
+              instanceSize: INSTANCE_SIZE,
+              nodeCount: 3,
+            },
+          }],
+        },
+      ],
+      name: PROJECT_NAME,
+    },
+    projectProps: {
+      orgId: ORG_ID,
+      name: PROJECT_NAME,
+    },
+    ipAccessListProps: {
+      accessList: [
+        {
+          ipAddress: ACCESS_LIST_IP_ADDR,
+          comment: ACCESS_LIST_COMMENT,
+        },
+      ],
+    },
+  };
+
+  const privateEndpointProps: PrivateEndpointProps = {
+    privateEndpoints: [{
+      vpcId: AWS_VPC_ID,
+      subnetIds: [AWS_SUBNET_ID],
+    }],
+  };
+
+  new atlasBasicPrivateEndpoint.AtlasBasicPrivateEndpoint(stack, 'testing-stack', {
+    atlasBasicProps: atlasBasicProps,
+    privateEndpointProps: privateEndpointProps,
+    region: REGION,
+  });
+
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties(RESOURCE_NAME_PROJECT, {
+    OrgId: ORG_ID,
+    Name: PROJECT_NAME,
+  });
+
+  template.hasResourceProperties(RESOURCE_NAME_CLUSTER, {
+    ProjectId: { 'Fn::GetAtt': [Match.anyValue(), 'Id'] },
+    ClusterType: 'REPLICASET',
+    Name: PROJECT_NAME,
+    ReplicationSpecs: [{
+      NumShards: 3,
+      AdvancedRegionConfigs: [{
+        RegionName: REGION,
+        ElectableSpecs: {
+          InstanceSize: INSTANCE_SIZE,
+          NodeCount: 3,
+        },
+      }],
+    }],
+  });
+
+  template.hasResourceProperties(RESOURCE_NAME_DB_USER, {
+    ProjectId: { 'Fn::GetAtt': [Match.anyValue(), 'Id'] },
+    DatabaseName: ADMIN_DB,
+    Password: PWD,
+    Username: DATABASE_USER_NAME,
+    Roles: [
+      {
+        DatabaseName: ADMIN_DB,
+        RoleName: ROLE_NAME,
+      },
+    ],
+  });
+
+  template.hasResourceProperties(RESOURCE_NAME_PROJECT_IP_ACCESS_LIST, {
+    ProjectId: { 'Fn::GetAtt': [Match.anyValue(), 'Id'] },
+    AccessList: [
+      {
+        Comment: ACCESS_LIST_COMMENT,
+        IPAddress: ACCESS_LIST_IP_ADDR,
+      },
+    ],
+  });
+
+  template.hasResourceProperties(RESOURCE_NAME_PRIVATE_ENDPOINT, {
+    GroupId: { 'Fn::GetAtt': [Match.anyValue(), 'Id'] },
+    Region: REGION,
+    PrivateEndpoints: [
+      {
+        VpcId: AWS_VPC_ID,
+        SubnetIds: [
+          AWS_SUBNET_ID,
+        ],
+      },
+    ],
+  });
+});
