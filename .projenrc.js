5beb6c710a01d25a893d5ea1667f19ededa5f05e
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-04-05 13:35:59 +0100

    Updated projen.js

--- .projenrc.js
+++ .projenrc.js
@@ -1,3 +1,49 @@
+const pullRequestTeamplateString = `<!--
+Thanks for contributing to MongoDB Atlas AWS CDK Resources!
+Before you submit your pull request, please review our contribution guidelines:
+https://github.com/mongodb/awscdk-resources-mongodbatlas/blob/master/CONTRIBUTING.md
+Please fill out the information below to help speed up the review process
+and getting you pull request merged!
+-->
+
+## Proposed changes
+
+<!-- 
+Describe the big picture of your changes here and communicate why we should accept this pull request.
+If it fixes a bug or resolves a feature request, be sure to link to that issue. 
+-->
+_Jira ticket:_ CLOUDP-#
+
+Please include a summary of the fix/feature/change, including any relevant motivation and context.
+
+
+Link to any related issue(s): 
+
+
+<!--
+Check the boxes that apply. If you're unsure about any of them, don't hesitate to ask!
+We're here to help! This is simply a reminder of what we are going to look for before merging your code.
+-->
+## Type of change:
+
+- [ ] Bug fix (non-breaking change which fixes an issue)
+- [ ] New feature (non-breaking change which adds functionality)
+- [ ] Breaking change (fix or feature that would cause existing functionality to not work as
+  expected)
+- [ ] This change requires a documentation update
+
+## Required Checklist:
+
+- [ ] I have signed the [MongoDB CLA](https://www.mongodb.com/legal/contributor-agreement)
+- [ ] I have added tests that prove my fix is effective or that my feature works
+- [ ] I have added any necessary documentation (if appropriate)
+- [ ] I have run \`make fmt\` and formatted my code
+- [ ] I have tested the CDK constructor in a CFN stack. See [TESTING.md](../TESTING.md)
+
+## Further comments
+`;
+
+
 const { awscdk, javascript, JsonFile } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'MongoDB',
@@ -21,7 +67,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     'mongodb',
     'atlas'],
   npmAccess: javascript.NpmAccess.PUBLIC,
-  releaseToNpm: false,
+  releaseToNpm: true,
   sampleCode: false,
   peerDeps: [
     '@mongodbatlas-awscdk/atlas-basic@1.1.0',
@@ -44,6 +90,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     '@mongodbatlas-awscdk/private-endpoint@1.1.0',
   ],
   stability: 'experimental',
+  pullRequestTemplateContents: [pullRequestTeamplateString],
 });
 
 new JsonFile(project, 'cdk.json', {
