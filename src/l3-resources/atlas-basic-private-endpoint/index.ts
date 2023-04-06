7b12000d3d9c200053a2f8ca0106064b48c06288
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-04-06 18:51:07 +0100

    Update index.ts

--- src/l3-resources/atlas-basic-private-endpoint/index.ts
+++ src/l3-resources/atlas-basic-private-endpoint/index.ts
@@ -12,10 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// import {
-//   CfnPrivateEndpoint,
-//   PrivateEndpoint,
-// } from '@mongodbatlas-awscdk/private-endpoint';
 import { Construct } from 'constructs';
 import {
   CfnPrivateEndpoint,
