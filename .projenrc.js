520d8b4b1f9e09581a8119b2932c9d514770421b
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-04-19 15:27:05 +0100

    INTMDB-762: fix release process. Run eslint

--- .projenrc.js
+++ .projenrc.js
@@ -70,7 +70,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     'atlas'],
   npmAccess: javascript.NpmAccess.PUBLIC,
   majorVersion: 1,
-  releaseTrigger: ReleaseTrigger.manual({gitPushCommand: ""}),
+  releaseTrigger: ReleaseTrigger.manual({ gitPushCommand: '' }),
   docgen: true,
   releaseToNpm: true,
   publishToNuget: {
