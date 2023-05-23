1cf96fef7693609d40622937d8303ba7f0bff110
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-05-23 09:14:42 +0100

    fixed lint

--- examples/mern-cdk-ci-cd/mern/server/server.sh
+++ examples/mern-cdk-ci-cd/mern/server/server.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-export temp_atlas_url=`aws cloudformation --region us-east-1 describe-stacks --stack-name AtlasIntegInfraStack --query "Stacks[0].Outputs[0].OutputValue"`
-export temp_atlas_password=`aws cloudformation --region us-east-1 describe-stacks --stack-name AtlasIntegInfraStack --query "Stacks[0].Outputs[1].OutputValue"`
-export temp_atlas_username=`aws cloudformation --region us-east-1 describe-stacks --stack-name AtlasIntegInfraStack --query "Stacks[0].Outputs[2].OutputValue"`
+temp_atlas_url=$(aws cloudformation --region us-east-1 describe-stacks --stack-name AtlasIntegInfraStack --query "Stacks[0].Outputs[0].OutputValue")
+temp_atlas_password=$(aws cloudformation --region us-east-1 describe-stacks --stack-name AtlasIntegInfraStack --query "Stacks[0].Outputs[1].OutputValue")
+temp_atlas_username=$(aws cloudformation --region us-east-1 describe-stacks --stack-name AtlasIntegInfraStack --query "Stacks[0].Outputs[2].OutputValue")
 
-export atlas_url=`echo $temp_atlas_url | cut -c 16-| tr -d '"'`
-export atlas_username=`echo $temp_atlas_username | tr -d '"'`
-export atlas_password=`echo $temp_atlas_password | tr -d '"'`
+atlas_url=$(echo $temp_atlas_url | cut -c 16-| tr -d '"')
+atlas_username=$(echo $temp_atlas_username | tr -d '"')
+atlas_password=$(echo $temp_atlas_password | tr -d '"')
 
-export ATLAS_URI="mongodb+srv://${atlas_username}:${atlas_password}@${atlas_url}"
+ATLAS_URI="mongodb+srv://${atlas_username}:${atlas_password}@${atlas_url}"
+
+export temp_atlas_url
+export temp_atlas_password
+export temp_atlas_username
+export atlas_url
+export atlas_username
+export atlas_password
+export ATLAS_URI
 
 npm run start &
