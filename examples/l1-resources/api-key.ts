7da2247762f8d5944881f4a81bb118cbfa344254
Adelmar92 <adelmar92@gmail.com>
2023-09-06 09:31:19 -0500

    pull requests refactors

--- examples/l1-resources/api-key.ts
+++ examples/l1-resources/api-key.ts
@@ -2,8 +2,6 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from 'constructs';
 import { CfnApiKey,CfnApiKeyProps,ProjectAssignment,ListOptions} from 'awscdk-resources-mongodbatlas';
 
-
-
 const app = new cdk.App();
 
 interface AtlasStackProps {
@@ -11,44 +9,36 @@ interface AtlasStackProps {
   readonly profile: string;
   readonly description: string;
   readonly awsSecretName: string;
+  readonly projectId: string;
 }
-const proAssignments: ProjectAssignment[] = [{
-    "projectId": "64ee3fcfaa95ad6b7e358d56",
-    "roles": [
-      "GROUP_READ_ONLY"
-    ]
-  },
-  {
-    "projectId": "64ee3fd58bb58153335e8469",
-    "roles": [
-      "GROUP_SEARCH_INDEX_EDITOR",
-      "GROUP_OWNER",
-      "GROUP_READ_ONLY"
-    ]
-  }];
 
-const listOptions : ListOptions = {
-    "pageNum": 1,
-    "itemsPerPage": 300
-} 
 const Roles = ["ORG_MEMBER","ORG_GROUP_CREATOR"]
 export class CdkTestingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const atlasProps = this.getContextProps();
-    const apiKeyProps = new CfnApiKey(this, 'api-key', {
+    new CfnApiKey(this, 'api-key', {
       profile: atlasProps.profile,
       orgId: atlasProps.orgId,
       description: atlasProps.description,
       awsSecretName: atlasProps.awsSecretName,
-      proAssignments: proAssignments,
-      listOptions: listOptions,
+      proAssignments: [{
+        projectId: atlasProps.projectId,
+        "roles": [
+          "GROUP_READ_ONLY"
+        ]
+      }],
       roles: Roles
     });
   }
 
   getContextProps(): AtlasStackProps {
+    const projectId = this.node.tryGetContext('projId');
+    if (!projectId){
+      throw "No context value specified for projId. Please specify via the cdk context."
+    }
+
     const orgId = this.node.tryGetContext('orgId');
     if (!orgId){
       throw "No context value specified for projId. Please specify via the cdk context."
@@ -61,6 +51,7 @@ export class CdkTestingStack extends cdk.Stack {
         profile,
         description,
         awsSecretName,
+        projectId
     }
   }
 }
