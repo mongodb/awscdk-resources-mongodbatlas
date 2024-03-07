8277fc29efaf2621b32ef20c76dae86bcdfe4639
Agustin Bettati <bettatiagustin@gmail.com>
2024-03-07 17:59:14 +0100

    fix unit test

--- test/l1-resources/search-deployment/index.test.ts
+++ test/l1-resources/search-deployment/index.test.ts
@@ -1,6 +1,6 @@
 // Copyright 2023 MongoDB Inc
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -23,32 +23,33 @@ const CLUSTER_NAME = "Cluster0";
 const INSTANCE_SIZE = "S30_HIGHCPU_NVME";
 const NODE_COUNT = 3;
 
-test("SearchDeployment construct should contain default properties", () => {
+test("CfnSearchDeployment construct should contain default properties", () => {
   const mockApp = new App();
   const stack = new Stack(mockApp);
 
-  new CfnSearchDeployment(stack, "search-deployment-stack", {
+  new CfnSearchDeployment(stack, "testing-stack", {
     profile: PROFILE,
     clusterName: CLUSTER_NAME,
     projectId: PROJECT_ID,
     specs: [
-        {
-          instanceSize: INSTANCE_SIZE,
-          nodeCount: NODE_COUNT,
-        },
-    ]
+      {
+        instanceSize: INSTANCE_SIZE,
+        nodeCount: NODE_COUNT,
+      },
+    ],
   });
 
   const template = Template.fromStack(stack);
+
   template.hasResourceProperties(RESOURCE_NAME, {
     Profile: PROFILE,
     ClusterName: CLUSTER_NAME,
     ProjectId: PROJECT_ID,
     Specs: [
-        {
-          InstanceSize: INSTANCE_SIZE,
-          NodeCount: NODE_COUNT,
-        },
-    ]
+      {
+        InstanceSize: INSTANCE_SIZE,
+        NodeCount: NODE_COUNT,
+      },
+    ],
   });
 });
